dbdee3915b95a261fd396fdda93fbe01c13e830b
lirco <lirco77@gmail.com>
2015-02-08 16:43:49 +0200

    fetching data from db

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,9 +3,30 @@
 /**
  * Module dependencies.
  */
+var MongoClient = require('mongodb').MongoClient;
+
+
 exports.index = function(req, res) {
-	res.render('index', {
-		user: req.user || null,
-		request: req
-	});
+  res.render('index', {
+    user: req.user || null,
+    request: req
+  });
+};
+
+exports.getData = function(req, res) {
+
+  MongoClient.connect('mongodb://localhost/dashboard-dev', function(err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('test_insert');
+
+      // Locate all the entries using find
+      collection.find().toArray(function(err, results) {
+        res.send(results);
+        // Let's close the db
+        db.close();
+      });
+
+  });
+
 };
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,22 @@
 'use strict';
 
+(function () {
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
-]);
+  function homeController($http, Authentication) {
+    var self = this;
+    self.authentication = Authentication;
+    self.resultsMongo = null;
+
+    $http({
+      method:'get',
+      url:'http://localhost:3000/getData'
+    })
+      .then(function(response) {
+        self.resultsMongo = response.data;
+      })
+  }
+
+  angular.module('core')
+    .controller('homeController', ['$http' ,'Authentication', homeController])
+
+}());
